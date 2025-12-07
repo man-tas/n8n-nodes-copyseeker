e7ddb594a4cc6f05c6f10d3882582684921c8a87
Mantas <m.zirgulis@gmail.com>
2025-12-07 20:43:08 +0200

    Update CopyseekerApi.credentials.ts

--- credentials/CopyseekerApi.credentials.ts
+++ credentials/CopyseekerApi.credentials.ts
@@ -1,4 +1,6 @@
 import {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
 	ICredentialType,
 	INodeProperties,
 } from 'n8n-workflow';
@@ -9,15 +11,34 @@ export class CopyseekerApi implements ICredentialType {
 	documentationUrl = 'https://copyseeker.net';
 	properties: INodeProperties[] = [
 		{
-			displayName: 'RapidAPI Key',
+			displayName: 'API Key',
 			name: 'apiKey',
 			type: 'string',
-			typeOptions: {
-				password: true,
-			},
+			typeOptions: { password: true },
 			default: '',
 			required: true,
 			description: 'Your RapidAPI key for Copyseeker',
 		},
 	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				'X-RapidAPI-Key': '={{$credentials.apiKey}}',
+				'X-RapidAPI-Host': 'reverse-image-search-by-copyseeker.p.rapidapi.com',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://reverse-image-search-by-copyseeker.p.rapidapi.com',
+			url: '/',
+			method: 'GET',
+			qs: {
+				imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/280px-PNG_transparency_demonstration_1.png',
+			},
+		},
+	};
 }
